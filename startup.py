80672e48f1e0def5b3c13b052ad4cd8cd54e9d9c
loyzious <85217451+loyzious@users.noreply.github.com>
2022-12-19 21:55:01 +0100

    streamlit update

--- startup.py
+++
@@ -1,9 +0,0 @@
-import dashboard
-import FastAPI
-import multiprocessing
-jobs = []
-jobs.append(multiprocessing.Process(target=FastAPI.start))
-jobs.append(multiprocessing.Process(target=dashboard.start))
-
-for j in jobs:
-    j.start()
